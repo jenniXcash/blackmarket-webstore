4bf12669af5b2ca18d45aadd7e64e8e384c901f4
jenDoeMoldovan <michalevic@gmail.com>
2021-10-23 14:50:22 +0300

    first steps of a webstore exercise

---
+++ src/Products.js/Products.js
@@ -0,0 +1,14 @@
+import Product from "../Product/Product";
+
+export default function Products() {
+  return (
+    <>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
